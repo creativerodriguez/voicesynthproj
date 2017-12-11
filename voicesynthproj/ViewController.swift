9e29b2d2d8d0ced331a94ea064c5b5b842e087aa
Orlando Rodriguez <creativerodriguez@gmail.com>
2017-12-11 13:14:01 -0500

    Initial Commit

---
+++ voicesynthproj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voicesynthproj
+//
+//  Created by Orlando Rodriguez on 12/11/17.
+//  Copyright © 2017 Creative Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
