350d04c798b7a39fdbfa86fcd7c972d7939f6f90
Orlando Rodriguez <creativerodriguez@gmail.com>
2017-12-11 18:11:34 -0500

    voice synth proj created
    voice synth proj created

--- voicesynthproj/ViewController.swift
+++ voicesynthproj/ViewController.swift
@@ -7,9 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
+
+enum modes {
+    case ADDITION
+    case SUBTRACTION
+    case MUTIPLICATION
+    case NOT_SET
+    
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var label: UILabel!
+    var labelString:String = "0"
+    var currentMode:modes = modes.NOT_SET
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    var say = AVSpeechUtterance(string: "")
+    
+    func sayString(str:String) {
+        say = AVSpeechUtterance(string: str)
+        say.rate = 0.5
+        say.voice = AVSpeechSynthesisVoice(language: "en-AU")
+        guard synth.isSpeaking else
+        {
+            synth.speak(say)
+            return
+        }
+    }
+    
+    @IBAction func didHitShit(sender: UIButton) {
+        self.didScore(points: 1)
+        sayString(str: "Hit that Shit mother fucker!")
+        
+    }
+    
+    @IBAction func didHitAlien(sender: UIButton) {
+        didScore(points: 5)
+        sayString(str: "John Buresh.  Hey Buresh.    What the hell you looking at?    Why do you grow tomatoes.   I hate tomatoes.    Hold up! Someone better call the cops tonight, because I'm about to get rat tat tat on a mother fucker! I'm Gangsta Siri, better recognize.")
+    }
+    
+    func didScore(points:Int) {
+        savedNum += points
+        label.text = "Score: \(savedNum)"
+    }
+    
+    
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
+    @IBAction func textToSpeech(sender: UIButton)
+    {
+        sayString(str: "Hey John Jones.    What the hell you looking at?    Better turn your eyes before they get snatched.    Someone better call the cops tonight, because I'm about to get rat tat tat on a mother fucker!")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +76,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
